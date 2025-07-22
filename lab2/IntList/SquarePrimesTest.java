62db0e48e536c9d22d54cb47787eb35b4600d207
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-22 13:50:37 +0530

    add more test cases

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,20 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesNoChange() {
+        IntList lst = IntList.of(1, 10, 20);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 10 -> 20", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSqurePrimesComples() {
+        IntList lst = IntList.of(5, 6, 7, 10, 11, 12, 13, 19, 27, 37);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("25 -> 6 -> 49 -> 10 -> 121 -> 12 -> 169 -> 361 -> 27 -> 1369", lst.toString());
+        assertTrue(changed);
+    }
 }
