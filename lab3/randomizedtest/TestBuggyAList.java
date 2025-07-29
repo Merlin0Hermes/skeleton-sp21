35bb190e3a9bb8c0344fa3e887a8faebfa18fb58
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-29 17:52:10 +0530

    add test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,5 +1,6 @@
 package randomizedtest;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -8,5 +9,23 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> list = new AListNoResizing<>();
+        BuggyAList<Integer> list2 = new BuggyAList<>();
+
+        list.addLast(3);
+        list2.addLast(3);
+
+        list.addLast(4);
+        list2.addLast(4);
+
+        list.addLast(5);
+        list2.addLast(5);
+
+        assertEquals(list.removeLast(), list2.removeLast());
+        assertEquals(list.removeLast(), list2.removeLast());
+        assertEquals(list.removeLast(), list2.removeLast());
+
+    }
 }
