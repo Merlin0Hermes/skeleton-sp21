ab4c096eabc705215532621087b21f7272f77932
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-07-29 18:08:17 +0530

    add test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -28,4 +28,35 @@ public class TestBuggyAList {
         assertEquals(list.removeLast(), list2.removeLast());
 
     }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                BL.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                System.out.println("L size: " + L.size());
+                System.out.println("BL size: " + BL.size());
+                assertEquals(L.size(), BL.size());
+            }
+            else if (operationNumber == 2 && L.size() > 0) {
+                System.out.println("getLast(" + L.getLast() + ")");
+                assertEquals(L.getLast(), BL.getLast());
+            }
+            else if (operationNumber == 3 && L.size() > 0) {
+                System.out.println("removeLast(" + L.removeLast() + ")");
+                assertEquals(L.removeLast(), BL.removeLast());
+            }
+        }
+    }
 }
