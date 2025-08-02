011f62aed4f41944b936a404d33c4e5c787ff453
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-08-02 15:13:22 +0530

    add tests for equals method           add tests

--- proj1/deque/testDeque.java
+++ proj1/deque/testDeque.java
@@ -1,4 +1,35 @@
 package deque;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class testDeque {
+
+    @Test
+    public void testEquals() {
+        Deque<Integer> list = new LinkedListDeque<>();
+        Deque<Integer> arr = new ArrayDeque<>();
+
+        list.addLast(10);
+        arr.addLast(10);
+        assertEquals(list, arr);
+
+        list.addFirst(5);
+        assertNotEquals(list, arr);
+
+        arr.addFirst(5);
+        assertEquals(list, arr);
+
+        for (int i = 0; i < 1000; ++i) {
+            arr.addLast(i);
+            list.addLast(i);
+        }
+        for (int i = 0; i < 1000; ++i) {
+            list.addFirst(i);
+            arr.addFirst(i);
+        }
+        assertEquals(list, arr);
+
+    }
 }
